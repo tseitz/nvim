6ca066f2309f128a17652c716ccef879f5e534fc
SeitzTegan <SeitzTegan@prahs.com>
2023-08-14 12:01:35 -0500

    chore(vim-test.lua): comment out unused keymap and file pattern configurations feat(vim-test.lua): set test runner to pytest for Python tests

--- after/plugin/vim-test.lua
+++ after/plugin/vim-test.lua
@@ -5,3 +5,7 @@ vim.keymap.set("n", "<leader>T", ":TestFile<CR>", opts)
 vim.keymap.set("n", "<leader>A", ":TestSuite<CR>", opts)
 vim.keymap.set("n", "<leader>l", ":TestLast<CR>", opts)
 --vim.keymap.set("n", "<leader>g", ":TestLast<CR>", opts)
+
+--vim.g["test#python#pytest#file_pattern"] = '**/test_*.py'
+--vim.g["test#python#behave#file_pattern"] = '**/*_steps.py'
+vim.g["test#python#runner"] = 'pytest'
